c01c8baac0f788574fdfe713bdb9618f42620bf4
OliverMoscow <56978722+OliverMoscow@users.noreply.github.com>
2023-11-22 11:51:51 -0500

    Initial Commit

---
+++ Percent Calculator/Percent_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Percent_CalculatorApp.swift
+//  Percent Calculator
+//
+//  Created by Oliver Moscow on 11/22/23.
+//
+
+import SwiftUI
+
+@main
+struct Percent_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
